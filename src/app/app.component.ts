0689e723bb5e75493b8eecf78fe1cd30a3557718
Bram Borggreve <borggreve@gmail.com>
2018-05-20 06:25:05 -0500

    feat: update examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
-import { UpploadSettings } from 'uppload-angular';
+import { UpploadEvents, UpploadSettings } from 'uppload-angular';
 
 @Component({
   selector: 'app-root',
   template: `
+    <pre> Modal Open: {{modalOpen}} Page: {{upploadPage}} </pre>
     <div class="container">
       <h1>Hello from Angular</h1>
       <p>Your image URL is: {{imageUrl}}</p>
-      <uppload [settings]="settings" (event)="handleEvent($event)">
+      <uppload #uppload [settings]="settings" (event)="handleEvent($event)">
         <button class="btn" data-uppload-button>Upload</button>
       </uppload>
+      <br>
+      <button class="btn"
+              (click)="openCamera(uppload)">Open Camera
+      </button>
+      <br>
+      <button class="btn"
+              (click)="openClose(uppload)">Open link page
+      </button>
+      (closes after timeout)
     </div>
   `,
   styles: [ `
@@ -32,15 +42,20 @@ import { UpploadSettings } from 'uppload-angular';
 export class AppComponent {
 
   public imageUrl: string;
+  public modalOpen = false;
+  public upploadPage: string;
+
+  avatarSize = [200, 200];
 
   public settings: UpploadSettings = {
+    crop: { aspectRatio: 1, maxSize: [ 300, 300 ], minSize: [ 100, 100 ] },
     minimumDelay: 2000,
-    isFileSizeAllowed: (params: any) => {
-      console.log('Custom size check', params);
+    isFileSizeAllowed: ({ size }) => {
+      console.log('Custom size check', size);
       return true;
     },
-    isFileTypeAllowed: (params: any) => {
-      console.log('Custom type check', params);
+    isFileTypeAllowed: ({ type }) => {
+      console.log('Custom type check', type);
       return true;
     },
     uploadFunction: () => {
@@ -53,10 +68,27 @@ export class AppComponent {
 
   handleEvent({ event, payload }) {
     switch (event) {
-      case 'fileUploaded':
+      case UpploadEvents.fileUploaded:
         return this.imageUrl = payload;
+      case UpploadEvents.modalClosed:
+        this.upploadPage = '';
+        return this.modalOpen = false;
+      case UpploadEvents.modalOpened:
+        return this.modalOpen = true;
+      case UpploadEvents.pageChanged:
+        return this.upploadPage = payload;
       default:
         return console.log(`Unhandled event`, { event, payload });
     }
+  }
+
+  openCamera(uppload) {
+    uppload.openModal();
+    uppload.changePage('camera');
+  }
+  openClose(uppload) {
+    uppload.openModal();
+    uppload.changePage('link');
+    setTimeout(() => uppload.closeModal(), 3000);
   }
 }
